6a6bb7bec32c5d9f8acd1c02b02b590370c74e66
bigcoso <abbase101@gmail.com>
2015-08-02 11:55:39 +0200

    Pagina inicial
    Signed-off-by: bigcoso

---
+++ SimpleCalculatorGWTp/src/com/calculatorgwt/client/SimpleCalculatorGWTp.java
@@ -0,0 +1,22 @@
+package com.calculatorgwt.client;
+
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public class SimpleCalculatorGWTp implements EntryPoint {
+
+	
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
