75f9945b85da07efa615a6f37a27678c542a3ad0
bigcoso <abbase101@gmail.com>
2015-08-02 19:43:16 +0200

    Pruebas

--- SimpleCalculatorGWTp/src/com/calculatorgwt/client/SimpleCalculatorGWTp.java
+++ SimpleCalculatorGWTp/src/com/calculatorgwt/client/SimpleCalculatorGWTp.java
@@ -9,14 +9,39 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.sencha.gxt.widget.core.client.button.TextButton;
 
+
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
 public class SimpleCalculatorGWTp implements EntryPoint {
 
-	
+	/**
+	 * This is the entry point method.
+	 */
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
 		
+		 //TextButton testButton=new TextButton("Prueba");
+		Calculator calculadora=new Calculator();
+		 
+		/* VerticalPanel vpNumeros=new VerticalPanel();
+		 
+		 for (int i=1;i<=9;i++)
+		 {
+			 TextButton testButton= new TextButton();
+			 testButton.setText(String.valueOf(i));
+			 vpNumeros.add(testButton);
+			 
+		 }
+		 RootPanel.get("calculatorContainer").add(vpNumeros);*/
+		  
 	}
 
 }
